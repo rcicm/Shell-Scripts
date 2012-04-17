19c3e1cef17e1a9080cdd8b069b726f1543bbda4
Rafael Inocencio <rafael_rci@yahoo.com.br>
2012-04-17 10:44:29 -0300

    Changing Postfix scripts

--- Postfix/delQueueMsg.sh
+++ Postfix/delQueueMsg.sh
@@ -2,13 +2,19 @@
 #############################################
 #
 # Delete messages from a specific sender from
-# a postfix Queue (Should run as root)
+# a postfix Queue 
 #
 # 2012/04/16 - Rafael Campos
 #############################################
+
+if [ $? != 1 ]
+then
+        echo "Usage: $0 SENDER (Ex.: $0 test@test.com)"
+        exit 1;
+fi
 
 SENDER=$1
 MONTH=`/bin/date +%b`
 
 echo "Removing from the Queue, the messages from $SENDER:"
-for msg in $(/usr/sbin/postqueue -p |grep $MONTH |grep $SENDER |awk '{print $1}' |sed -e 's/*//g') ; do /usr/sbin/postsuper -d $msg ; done
+for msg in $(/usr/sbin/postqueue -p |grep $MONTH |grep $SENDER |awk '{print $1}' |sed -e 's/*//g') ; do sudo /usr/sbin/postsuper -d $msg ; done
